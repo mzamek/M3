a91c814d20b6f7f94b2500b2c81820c4b6edb0c9
Mark Zamek <markzamek@gmail.com>
2023-02-13 15:16:31 -0800

    second game menu done

---
+++ Assets/Scripts/PauseMenuToggle.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class PauseMenuToggle : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (canvasGroup.interactable)
+            {
+                canvasGroup.interactable = false; 
+                canvasGroup.blocksRaycasts = false; 
+                canvasGroup.alpha = 0f;
+            }
+            else { 
+                canvasGroup.interactable = true; 
+                canvasGroup.blocksRaycasts = true; 
+                canvasGroup.alpha = 1f; 
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        if(canvasGroup == null)
+        {
+            Debug.LogError("canvasGroup is null");
+        }
+    }
+}
